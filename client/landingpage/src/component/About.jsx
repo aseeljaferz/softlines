0cecda78673749b339c05de7a749d3c930808c2c
AathavanSeralathan001 <88584371+AathavanSeralathan001@users.noreply.github.com>
2023-05-02 23:14:45 +0530

    update about.jsx

--- client/landingpage/src/component/About.jsx
+++ client/landingpage/src/component/About.jsx
@@ -5,15 +5,6 @@ import './about.css'
 
 let easeing = [0.6, -0.05, 0.01, 0.99];
 
-const stagger = {
-  animate:{
-    transition:{
-      delayChildren:0.4,
-      staggerChildren:0.2,
-      staggerDirection:1
-    }
-  }
-}
 
 const fadeInUp = {
   initial:{
@@ -72,7 +63,6 @@ const fadeInUpContent = {
   }
 };
 
-const transition = {duration:1.4, ease:[0.6, 0.01,-0.05,0.9]}
 
 const header ={
   initial:{
